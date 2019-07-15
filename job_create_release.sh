a8ce59f24209a4e72bc0c04d582b7bf504d708b1
mgoerke <mgoerke@evoila.de>
2019-07-15 12:03:03 +0200

    updatet permissions (+x)

--- job_create_release.sh
+++ job_create_release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 fly -t ci set-pipeline -c pipeline.yml -p nginx-release -l creds.yml
-fly -t ci trigger-job -j nginx-release/create-release
+#fly -t ci trigger-job -j nginx-release/create-release
