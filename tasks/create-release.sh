c1407e0f1874683314e0fff5446432e04c6207e8
mgoerke <mgoerke@evoila.de>
2019-07-16 13:27:49 +0200

    put deploy-release into own job, matched files, created needed tasks

--- tasks/create-release.sh
+++ tasks/create-release.sh
@@ -5,4 +5,3 @@ touch keyval/keyval.properties
 nginx-pipeline/scripts/connect-director.sh
 nginx-pipeline/scripts/blobs.sh
 nginx-pipeline/scripts/upload-release.sh
-nginx-pipeline/scripts/deploy-release.sh
