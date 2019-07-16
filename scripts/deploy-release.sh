9f03f9b340e6e0ade10cc3098193d4f57753c0b6
mgoerke <mgoerke@evoila.de>
2019-07-16 14:27:06 +0200

    polish

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -2,7 +2,8 @@
 bosh alias-env $BOSH_ALIAS -e $BOSH_IP
 cd nginx-release
 bosh deploy -d nginx manifest.yml -n
-bosh deployments
-echo "####################################"
-echo "visit nginx under $URL_NGINX_RELEASE"
-echo "####################################"
+
+echo "visit nginx under:"
+
+bosh -d nginx vms
+
