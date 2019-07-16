1cb7d6a6eda6d127a088e74337416e7a46751a2b
mgoerke <mgoerke@evoila.de>
2019-07-16 11:53:39 +0200

    add directory and file for keyval ressource

--- tasks/create-release.sh
+++ tasks/create-release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
+mkdir keyval
+touch keyval/keyval.properties
 nginx-pipeline/scripts/connect-director.sh
 nginx-pipeline/scripts/blobs.sh
 nginx-pipeline/scripts/upload-release.sh
