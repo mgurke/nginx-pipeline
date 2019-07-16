443c27252aafbb38430bbb86ab25e6f403a5664a
mgoerke <mgoerke@evoila.de>
2019-07-16 09:20:15 +0200

    deletet unecessary files

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone git@bitbucket.org:meshstack/mg-kt-cf.git
-cd mg-kt-cf/nginx-release
-ls -la
-bosh delete-deployment -d nginx -n
-bosh create-release --force 
-bosh upload-release
-bosh deploy -d nginx manifest.yml -n
-bosh tasks --recent
