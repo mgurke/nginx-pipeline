0bff121102a0978ee4fd3f7a76199f3109137876
mgoerke <mgoerke@evoila.de>
2019-07-16 10:19:01 +0200

    corrected file path, added mkdir src because it's missing

--- scripts/blobs.sh
+++ scripts/blobs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 cd nginx-release
+mkdir src
 wget $URL_NGINX_RELEASE -O nginx-$NGINX_VERSION.tar.gz
 wget $URL_PCRE_RELEASE -O pcre-$PCRE_VERSION.tar.gz
 
-bosh add-blob nginx-* nginx-$NGINX_VERSION.tar.gz
-bosh add-blob pcre-* pcre-$PCRE_VERSION.tar.gz
+bosh add-blob nginx-* nginx_$NGINX_VERSION/nginx-$NGINX_VERSION.tar.gz
+bosh add-blob pcre-* pcre_$PCRE_VERSION/pcre-$PCRE_VERSION.tar.gz
 
 bosh blobs
