a940dceca475076626ce8d1e6fcc65b9f204fec2
mgoerke <mgoerke@evoila.de>
2019-07-17 11:33:55 +0200

    added test for deployment, writes VM IP in keyval file

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 bosh alias-env $BOSH_ALIAS -e $BOSH_IP
 cd nginx-release
-bosh deploy -d nginx manifest.yml -n
+bosh deploy -d $DEPLOYMENT_NAME manifest.yml -n
 
+
+
+
+# Find and write the IP of the nginx vm to the keyval file
+IP=$(bosh vms -d $DEPLOYMENT_NAME| awk '{print $4}')
 echo "visit nginx under:"
+echo IP="$IP" >> ${ROOT_FOLDER}/keyval/keyval.properties
 
-bosh -d nginx vms
 
+
+props="${ROOT_FOLDER}/keyval/keyval.properties"
+if [ -f "$props" ]
+then
+  echo "Reading passed key values"
+  while IFS= read -r var
+  do
+    if [ ! -z "$IP" ]
+    then
+      echo "Adding: $IP"
+      export "$IP"
+    fi
+  done < "$props"
+fi
+
+
+cat ${ROOT_FOLDER}/keyval/keyval.properties
