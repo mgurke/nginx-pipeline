6e5db9a2365b0e764ad761895906d5fd9d4b1221
mgoerke <mgoerke@evoila.de>
2019-07-22 11:31:28 +0200

    edited port

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -3,11 +3,10 @@ apt-get -y update
 apt-get -y install curl
 
 PROPERTIES_FILE=trigger-test/keyval.properties
-TEST_PORT=8080
+TEST_PORT=80
 
 # Searches the properties file for the IP and writes the value into a variable
 IP=$(cat $PROPERTIES_FILE | grep IP= | cut -d'=' -f2)
-echo 
 echo $IP
 # Test the reachability of the nginx
-curl -f $IP:8080    
+curl -f $IP:$TEST_PORT
