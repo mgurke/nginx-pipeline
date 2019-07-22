0bd09d717460026a587da49f0a8edeee16bd6c42
mgoerke <mgoerke@evoila.de>
2019-07-22 11:34:56 +0200

    added sleep so nginx can come up

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -9,4 +9,6 @@ TEST_PORT=80
 IP=$(cat $PROPERTIES_FILE | grep IP= | cut -d'=' -f2)
 echo $IP
 # Test the reachability of the nginx
+echo "Wait 30 sec for Nginx to come up"
+sleep 30
 curl -f $IP:$TEST_PORT
